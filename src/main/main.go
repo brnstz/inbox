90a40eda9cd854f484f1fb55781f2c03b02e6f6a
Brian Seitz <brnstz@gmail.com>
2013-02-17 12:37:11 -0500

    - bugfixes, workingish version

--- src/main/main.go
+++ src/main/main.go
@@ -8,16 +8,18 @@ import (
     "flag"
     "fmt"
     "reflect"
-    //"os"
+    "encoding/json"
+    "os"
+    "io/ioutil"
 )
 
 type Inbox struct {
-    Senders map[string]Sender
+    Senders map[string]*Sender
 }
 
 type Sender struct {
     Count int
-    Ids []int
+    Ids []uint32
 }
 
 
@@ -38,87 +40,79 @@ func login(server string, user string, pw string) (c *imap.Client, err error) {
     return c, nil
 }
 
-func fetchIds(c *imap.Client) (ids []uint32) {
+func fetchIds(c *imap.Client) (uids []uint32) {
     seq_set, err := imap.NewSeqSet("1:*")
 
     if (err != nil) {
         panic(err)
     }
 
-    cmd, err := imap.Wait(c.Fetch(seq_set, "FLAGS"))
+    cmd, err := imap.Wait(c.UIDFetch(seq_set, "FLAGS"))
 
     if (err != nil) {
         panic(err)
     }
 
     for _, row := range(cmd.Data) {
+        fmt.Println(row)
+        fmt.Println(row.MessageInfo().UID)
 
+        /*
         v, ok := row.Fields[0].(uint32)
         if (ok) {
             ids = append(ids, v)
         }
+        */
+        uids = append(uids, row.MessageInfo().UID)
     }
 
-    return ids
+    return uids
 }
 
 //func parseMessage
 
-func fetchMessage(c *imap.Client, id uint32) {
-    seq_set, err := imap.NewSeqSet(fmt.Sprintf("%d", id))
+func fetchMessage(c *imap.Client, uid uint32) (emailName string, emailAddr string) {
+    seq_set, err := imap.NewSeqSet(fmt.Sprintf("%d", uid))
 
     if err != nil {
         panic(err)
     }
 
-    cmd, err := imap.Wait(c.Fetch(seq_set, "ALL"))
+    cmd, err := imap.Wait(c.UIDFetch(seq_set, "ALL"))
+    rsp := cmd.Data[0]
+    fmt.Println(rsp.MessageInfo().Attrs["ENVELOPE"])
+    emailAddrInfo := imap.AsList(imap.AsList(imap.AsList(imap.AsList(rsp.MessageInfo().Attrs["ENVELOPE"])[2]))[0])
+    emailName = imap.AsString(emailAddrInfo[0])
+    emailAddr = fmt.Sprintf("%s@%s", imap.AsString(emailAddrInfo[2]), imap.AsString(emailAddrInfo[3]))
 
-    if err != nil {
-        panic(err)
-    }
-
-    /*
-    res, err := cmd.Result(1)
-
-    if err != nil {
-        panic(err)
-    }
-    */
-
-    //fmt.Println(reflect.TypeOf(cmd.Data[0].Fields[2]))
-    //fmt.Println(cmd.Data[0].Fields[2])
-    //fmt.Println(cmd.Data[0])
-    //fmt.Println(reflect.TypeOf(cmd.Data[0]))
-    //fmt.Println(reflect.TypeOf(imap.AsFieldMap(cmd.Data[0].Fields[2])["ENVELOPE"]))
-    //fmt.Println(imap.AsFieldMap(cmd.Data[0].Fields[2])["ENVELOPE"])
-    /*
-    var (
-        emailName string
-        emailAddr string
-        msgId int
-    )
-    */
-    /*
-    for k, v := range imap.AsList(imap.AsList(cmd.Data[0].Fields[2])[1]) {
-        fmt.Println(k, " ", v)
-    }
-    */
-    emailAddrInfo := imap.AsList(imap.AsList(imap.AsList(imap.AsList(cmd.Data[0].Fields[2])[1])[2])[0])
-    emailName := imap.AsString(emailAddrInfo[0])
-    emailAddress := fmt.Sprintf("%s@%s", imap.AsString(emailAddrInfo[2]), imap.AsString(emailAddrInfo[3]))
-    fmt.Println(emailName)
-    fmt.Println(emailAddress)
-
-    //fmt.Println(res)
-    //fmt.Println(res.Decoded)
-    //fmt.Println(cmd.String())
-
+    return emailName, emailAddr
 }
 
 func blah() {
 
     fmt.Println(reflect.TypeOf(1))
     _ = new(Inbox)
+    os.Exit(1)
+}
+
+func addToInbox(inbox *Inbox, uid uint32, emailName string, emailAddr string) {
+
+    if inbox.Senders == nil {
+        inbox.Senders = map[string]*Sender {}
+    }
+
+    full := fmt.Sprintf("%s %s", emailName, emailAddr)
+
+    sender, found := inbox.Senders[full]
+    if found {
+        sender.Count++
+        sender.Ids = append(sender.Ids, uid)
+    } else {
+        sender = new(Sender)
+        sender.Count = 1
+        sender.Ids = append(sender.Ids, uid)
+        inbox.Senders[full] = sender
+    }
 }
 
 func main() {
@@ -145,12 +139,23 @@ func main() {
         panic(err)
     }
 
-    ids := fetchIds(c)
+    uids := fetchIds(c)
 
-    for _, id := range(ids) {
-        fmt.Printf("ID SAYS: %d\n", id)
-        fetchMessage(c, id)
+    var emailAddr, emailName string
+    inbox := new(Inbox)
+    for _, uid := range(uids) {
+        fmt.Printf("UID SAYS: %d\n", uid)
+        emailName, emailAddr = fetchMessage(c, uid)
+        addToInbox(inbox, uid, emailName, emailAddr)
     }
+
+    b, err := json.Marshal(inbox)
+
+    if (err != nil) {
+        panic(err)
+    }
+
+    ioutil.WriteFile("inbox.json", b, 0644)
 
     /*
     res, err := cmd.Result(0)
