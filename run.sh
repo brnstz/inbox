442d3360388a97627854ad016015a4af2a53938d
Brian Seitz <brnstz@gmail.com>
2013-04-05 17:54:52 -0400

    - updating run.sh with IMAP_SERVER

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # Login details for IMAP server
 IMAP_USERNAME='FIXME'
 IMAP_PW='FIXME'
+IMAP_SERVER='FIXME'
 
 # Any integer. This is used to associate your IMAP data in the mongodb
 USER_ID=1234
@@ -10,4 +11,4 @@ USER_ID=1234
 export GOPATH=`pwd`
 
 # Runs sudo to run on port 80
-go build main && sudo ./main --server imap.gmail.com --user $IMAP_USERNAME --pw $IMAP_PW --user_id $USER_ID
+go build main && sudo ./main --server $IMAP_SERVER --user $IMAP_USERNAME --pw $IMAP_PW --user_id $USER_ID
